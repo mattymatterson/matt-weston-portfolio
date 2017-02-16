81be0bac9f6f7c23d44c68266390591110b21696
mattymatterson <matt.weston.mw@gmail.com>
2017-02-16 12:49:10 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,53 +1,37 @@
-function createNewChat(name, initials, chatPosOrig, amtChats, chatPosMod, chatOffset) {
-    var newChat = document.createElement('button');
-    newChat.id = initials + "Chat"
-    newChat.textContent = name;
-    newChat.className = 'online';
-    newChat.style.position = "fixed";
-    newChat.style.zIndex = "9999999999";
-    newChat.style.bottom = "0px";
-    newChat.style.right = 150 + (amtChats* chatPosMod) + chatOffset;
-    document.getElementById("chatContainer").appendChild(newChat);
-    amtChats += 1;
+$( document ).ready(function() {
+    var toggled = false;
 
-}
-function createChatBoxes(initials, amtChats, chatPosMod, chatOffset) {
-    var newChatBox = document.createElement('div');
-    newChatBox.id = initials + "chatBox";
-    newChatBox.className = 'onlineList';
-    newChatBox.style.position = "fixed";
-    newChatBox.style.right = 150 + (amtChats* chatPosMod) + chatOffset;
-    newChatBox.display = "none";
-}
-$(document).ready(function() {
-    var chatPosOrig = document.getElementById("chat").style.width;
-    var chatOffset = (Math.max(document.documentElement.clientWidth, window.innerWidth || 0)/100)*9;
-    var chatPosMod = 150;
-    var amtChats = 0;
-    $("#chat").click(function(){
-        $(".onlineList").toggle();
-    });
-    $("#mw").click(function(){
-        if (document.getElementById("mwChat") == null) {
-            createNewChat("Matt Weston", "mw", chatPosOrig, amtChats, chatPosMod, chatOffset);
-            amtChats +=1;
-            $("#mwChat")(function(){
-                alert("clicked!");
-            });
+    $( "#collapse-left-navbar" ).click(function() {
+        if (toggled == false){
+            var leftNav = $("#left-nav");
+            var Content = $(".container");
+            var ChatBox = $("#chatType")
+            leftNav.css("left","-270px");
+            Content.css("left","-270px");
+            Content.css("position","relative");
+            ChatBox.css("left","5vh");
+            ChatBox.css("width","50%");
+            toggled = true;
+        } else {
+            var leftNav = $("#left-nav");
+            var Content = $(".container");
+            var ChatBox = $("#chatType")
+            leftNav.css("left","0px");
+            Content.css("position","relative");
+            Content.css("left","0px");
+            ChatBox.css("left","35vh");
+            ChatBox.css("width","70%");
+            toggled = false;
         }
     });
-    $("#qc").click(function(){
-        if (document.getElementById("qcChat") == null) {
-            createNewChat("Quin Carter", "qc", chatPosOrig, amtChats, chatPosMod, chatOffset);
-            createChatBoxes("qc", amtChats, chatPosMod, chatOffset);
-            amtChats +=1;
-        }
+    $("#Profile").click(function(){
+        $("#profileMenu").toggle();
     });
-    $("#lm").click(function(){
-        if (document.getElementById("lmChat") == null) {
-            createNewChat("Logan McCourry", "lm",  chatPosOrig, amtChats, chatPosMod, chatOffset);
-            createChatBoxes("lm", amtChats, chatPosMod, chatOffset);
-            amtChats +=1;
+});
+$(document).keypress(function(e) {
+    if ($("#chatType").is(":focus")) {
+        if(e.which == 13) {
+            alert('You pressed enter!');
         }
-    });
+    }  
 });
