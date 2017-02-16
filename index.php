809f8d3c0037aa373bd2c08c2693d3b4815cd5e9
mattymatterson <matt.weston.mw@gmail.com>
2017-02-16 12:48:39 -0600

    Update index.php

--- index.php
+++ index.php
@@ -15,46 +15,7 @@ $username = "Matt Weston";
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="styles\test.css">
-
-    <script>
-        $( document ).ready(function() {
-            var toggled = false;
-
-            $( "#collapse-left-navbar" ).click(function() {
-                if (toggled == false){
-                    var leftNav = $("#left-nav");
-                    var Content = $(".container");
-                    var ChatBox = $("#chatType")
-                    leftNav.css("left","-270px");
-                    Content.css("left","-270px");
-                    Content.css("position","relative");
-                    ChatBox.css("left","5vh");
-                    ChatBox.css("width","50%");
-                    toggled = true;
-                } else {
-                    var leftNav = $("#left-nav");
-                    var Content = $(".container");
-                    var ChatBox = $("#chatType")
-                    leftNav.css("left","0px");
-                    Content.css("position","relative");
-                    Content.css("left","0px");
-                    ChatBox.css("left","35vh");
-                    ChatBox.css("width","70%");
-                    toggled = false;
-                }
-            });
-            $("#Profile").click(function(){
-                $("#profileMenu").toggle();
-            });
-        });
-        $(document).keypress(function(e) {
-            if ($("#chatType").is(":focus")) {
-                if(e.which == 13) {
-                    alert('You pressed enter!');
-                }
-            }  
-        });
-    </script>
+    <script src="/js/main.js"></script>
 </head>
 <body>
 <div class="nav-side-menu" id="left-nav">
