77c0593955676163d8e7e086a29480507aee8d11
mattymatterson <matt.weston.mw@gmail.com>
2017-02-16 21:35:51 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,28 +1,52 @@
 <?php
-$user = "kjuxctiwjuizkv";
-$pass = "f0e911e4e4cf90720283e28d02c0f26080d675133f65969fa30abad47e18f582";
+$dsn = "pgsql:"
+    . "host=ec2-107-20-191-76.compute-1.amazonaws.com;"
+    . "dbname=dc2ibd1t6ecgng;"
+    . "user=kjuxctiwjuizkv;"
+    . "port=5432;"
+    . "sslmode=require;"
+    . "password=f0e911e4e4cf90720283e28d02c0f26080d675133f65969fa30abad47e18f582";
+
+$db = new PDO($dsn);
+$query = "selet * from users";
+
+$result = $db->query($query);
+while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+    echo "<tr>";
+    echo "<td>" . $row["user_id"] . "</td>";
+    echo "<td>" . htmlspecialchars($row["username"]) . "</td>";
+    echo "<td>" . htmlspecialchars($row["password"]) . "</td>";
+    echo "<td>" . htmlspecialchars($row["firstname"]) . "</td>";
+    echo "<td>" . htmlspecialchars($row["lastname"]) . "</td>";
+    echo "<td>" . htmlspecialchars($row["email"]) . "</td>";
+    echo "</tr>";
+}
 ?>
 
-<?php
-# This function reads your DATABASE_URL config var and returns a connection
-# string suitable for pg_connect. Put this in your app.
-function pg_connection_string_from_database_url() {
-  extract(parse_url($_ENV["poop"]));
-  return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
-}
-# Here we establish the connection. Yes, that's all.
-$pg_conn = pg_connect(pg_connection_string_from_database_url());
-# Now let's use the connection for something silly just to prove it works:
-$result = pg_query($pg_conn, "SELECT relname FROM pg_stat_user_tables WHERE schemaname='public'");
-print "<pre>\n";
-if (!pg_num_rows($result)) {
-  print("Your connection is working, but your database is empty.\nFret not. This is expected for new apps.\n");
-} else {
-  print "Tables in your database:\n";
-  while ($row = pg_fetch_row($result)) { print("- $row[0]\n"); }
-}
-print "\n";
-?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 <?php
 //header("Location:mainpage.php");
 ?>
