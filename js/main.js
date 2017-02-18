9164bac9064a149275ed42a5fdb82c886008a6d1
mattymatterson <matt.weston.mw@gmail.com>
2017-02-18 09:58:54 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,38 +3,40 @@ $( document ).ready(function() {
     var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
     $( "#collapse-left-navbar" ).click(function() {
         if (w >= 766) { // if viewport is larger than 766
-            if (toggled == false){
+            if (toggled == false) {
                 var leftNav = $("#left-nav");
                 var Content = $(".container");
-                leftNav.css("left","-270px");
-                Content.css("left","-270px");
-                Content.css("position","relative");
+                leftNav.css("left", "-270px");
+                Content.css("left", "-270px");
+                Content.css("position", "relative");
                 toggled = true;
             } else {
                 var leftNav = $("#left-nav");
                 var Content = $(".container");
-                leftNav.css("left","0px");
-                Content.css("position","relative");
+                leftNav.css("left", "0px");
+                Content.css("position", "relative");
                 toggled = false;
-            } //end toggle
+            }
             if (w <= 765) {
                 { //if viewport is smaller than 765
-                if (toggled == false){
-                    var leftNav = $("#left-nav");
-                    var Content = $(".container");
-                    leftNav.css("left","-120px");
-                    Content.css("left","30px");
-                    Content.css("position","relative");
-                    toggled = true;
-                } else {
-                    var leftNav = $("#left-nav");
-                    var Content = $(".container");
-                    leftNav.css("left","0px");
-                    Content.css("position","relative");
-                    Content.css("left","0px");
-                    toggled = false;
-                } //end toggle
-        } //end if ln 5
+                    if (toggled == false) {
+                        var leftNav = $("#left-nav");
+                        var Content = $(".container");
+                        leftNav.css("left", "-120px");
+                        Content.css("left", "30px");
+                        Content.css("position", "relative");
+                        toggled = true;
+                    } else {
+                        var leftNav = $("#left-nav");
+                        var Content = $(".container");
+                        leftNav.css("left", "0px");
+                        Content.css("position", "relative");
+                        Content.css("left", "0px");
+                        toggled = false;
+                    }
+                }
+            }
+        }
     }); //end nav collapse
     $("#Profile").click(function(){
         $("#profileMenu").toggle();
@@ -45,5 +47,5 @@ $(document).keypress(function(e) {
         if(e.which == 13) {
             alert('You pressed enter!');
         }
-    }  
+    }
 });
