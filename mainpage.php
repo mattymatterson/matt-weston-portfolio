0aeb506795951d76ca4bbeb5b700edcc0d892f94
mattymatterson <matt.weston.mw@gmail.com>
2017-02-19 10:41:15 -0600

    Update mainpage.php

--- mainpage.php
+++ mainpage.php
@@ -19,6 +19,7 @@ if(!isset($_SESSION['username'])) {
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="styles\main.css">
     <script src="/js/main.js"></script>
+    <link rel="icon" href="images/mwicon.png">
 </head>
 <body>
 <div class="nav-side-menu" id="left-nav">
