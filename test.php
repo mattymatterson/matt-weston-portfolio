c82192820d1fbaad822be15dd9e9ab1e8f492fa1
mattymatterson <matt.weston.mw@gmail.com>
2017-02-19 09:57:11 -0600

    Update test.php

--- test.php
+++ test.php
@@ -1,20 +1,22 @@
-<?php // HTTP Headers for ZIP File Downloads
-// https://perishablepress.com/press/2010/11/17/http-headers-file-downloads/
+<?php 
+if(!isset($_SESSION['username'])) {
+    header("Location: login.html");
+} else {
+  // set example variables
+  $filename = "PowerShellForm.7z";
+  $filepath = "/";
 
-// set example variables
-$filename = "PowerShellForm.7z";
-$filepath = "/";
-
-// http headers for zip downloads
-header("Pragma: public");
-header("Expires: 0");
-header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
-header("Cache-Control: public");
-header("Content-Description: File Transfer");
-header("Content-type: application/octet-stream");
-header("Content-Disposition: attachment; filename=\"".$filename."\"");
-header("Content-Transfer-Encoding: binary");
-header("Content-Length: ".filesize($filepath.$filename));
-ob_end_flush();
-@readfile($filepath.$filename);
+  // http headers for zip downloads
+  header("Pragma: public");
+  header("Expires: 0");
+  header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
+  header("Cache-Control: public");
+  header("Content-Description: File Transfer");
+  header("Content-type: application/octet-stream");
+  header("Content-Disposition: attachment; filename=\"".$filename."\"");
+  header("Content-Transfer-Encoding: binary");
+  header("Content-Length: ".filesize($filepath.$filename));
+  ob_end_flush();
+  @readfile($filepath.$filename);
+}
 ?>
