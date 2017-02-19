726065a4b64b41a71c8f70a498384de3e3f2665e
mattymatterson <matt.weston.mw@gmail.com>
2017-02-19 14:24:08 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -58,6 +58,7 @@ $(document).keypress(function(e) {
     if ($("#chatType").is(":focus")) {
         if(e.which == 13) {
             $.post( "type.php",  { data: "Hello" }  );
+            
             $("#chatType").val("");
         }
     }
