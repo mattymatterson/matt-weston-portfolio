ec4089c2ac649468ddc3d6b4048351ea6eb0d251
mattymatterson <matt.weston.mw@gmail.com>
2017-02-19 18:04:47 -0600

    Update mainpage.php

--- mainpage.php
+++ mainpage.php
@@ -162,14 +162,15 @@ if(!isset($_SESSION['username'])) {
     </div>
 </div>
     <script>
-        $("#mattweston").click(function(){
-            <?php $_SESSION[chat_id] = 1; ?>
-        });
+        
         $("#quincarter").click(function(){
             <?php $_SESSION[chat_id] = 2; ?>
         });
         $("#loganmccourry").click(function(){
             <?php $_SESSION[chat_id] = 3; ?>
+        });
+        $("#mattweston").click(function(){
+            <?php $_SESSION[chat_id] = 1; ?>
         });
     </script>
 </body>
