db703c67b795c9aa003bd57bb22e59004c9b06ce
mattymatterson <matt.weston.mw@gmail.com>
2017-02-19 19:47:24 -0600

    Update type.php

--- type.php
+++ type.php
@@ -15,10 +15,13 @@ $db = new PDO($dsn);
 $query = "insert into chat(chat_id,message, chat_from, chat_to, time) values ( nextval('chat_id_seq'), 'Hello world', 1 , '0', 2:30);";
 $result = $db->query($query);
 
+$time = date("h:i:sa");
+$date = date("Y-m-d");
+$DateTime = $date . " " . $time;
 
 $db = pg_connect("host=ec2-107-20-191-76.compute-1.amazonaws.com port=5432 dbname=dc2ibd1t6ecgng user=kjuxctiwjuizkv password=f0e911e4e4cf90720283e28d02c0f26080d675133f65969fa30abad47e18f582");
 //$query = "INSERT INTO chat VALUES ('nextval('chat_id_seq')','$_POST[data]','$_SESSION['user_id'], '0', '2:30');";
-$query = "INSERT INTO chat VALUES (nextval('chat_id_seq'),'$_POST[data]','$_SESSION[user_id]', '$_SESSION[chat_id]', '2:30');";
+$query = "INSERT INTO chat VALUES (nextval('chat_id_seq'),'$_POST[data]','$_SESSION[user_id]', '$_SESSION[chat_id]', '$DateTime');";
 $result = pg_query($query); 
 
 /*
