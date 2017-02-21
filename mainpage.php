bdd2c24938ca9d7d429df9644fb19de88925d609
mattymatterson <matt.weston.mw@gmail.com>
2017-02-20 19:19:38 -0600

    Update mainpage.php

--- mainpage.php
+++ mainpage.php
@@ -105,8 +105,8 @@ if(!isset($_SESSION['username'])) {
                 $query = "select * from chat order by chat_id desc;";
                 $result = $db->query($query);
                 while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-
-                    echo "<td>" . htmlspecialchars($row["time"] . ": ") . "</td>";
+                    $newtime = explode(" ",$row["time"])
+                    echo "<td>" . htmlspecialchars($newtime[1] . ": ") . "</td>";
                     echo "<td>" . htmlspecialchars($row["message"]) . "</td><br>";
                     
                 }
