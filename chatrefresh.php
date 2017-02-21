42d281cc7684a70ed4b926473ea6fe22d24943d8
mattymatterson <matt.weston.mw@gmail.com>
2017-02-20 21:01:43 -0600

    Update chatrefresh.php

--- chatrefresh.php
+++ chatrefresh.php
@@ -1,9 +1,10 @@
 <?php
 session_start();
 
+$result = array("time", "name", "message");
 //$response = "time name","message";
 
 $fp = fopen('results.json', 'w');
-//fwrite($fp, json_encode($response));
-//fclose($fp);
+fwrite($fp, json_encode($result));
+fclose($fp);
 ?>
