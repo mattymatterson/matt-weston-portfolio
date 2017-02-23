9c0b42156cc8fc855fc95f85ae62f3d345083b75
mattymatterson <matt.weston.mw@gmail.com>
2017-02-22 21:12:49 -0600

    Update mainpage.php

--- mainpage.php
+++ mainpage.php
@@ -38,7 +38,7 @@ if(!isset($_SESSION['username'])) {
             $.post( "chat.php",  { data: "3" }  );
             $("#header-bar").text("Logan McCourry");
         });
-
+        $("#chat-box").scrollTop($("#chat-box")[0].scrollHeight);
     });
 </script>
 </head>
