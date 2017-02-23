3f6851510086f06a34b5763b19833f5a13483574
mattymatterson <matt.weston.mw@gmail.com>
2017-02-22 19:37:56 -0600

    Update mainpage.php

--- mainpage.php
+++ mainpage.php
@@ -101,13 +101,12 @@ if(!isset($_SESSION['username'])) {
                     $.getJSON("results.json", function(result){
                         $.each(result, function(i, field){
                             messages += field + "<br> "
-                            $("#chat-box").text("Dear <br/> Diary <br/> :D");
                             $.fn.multiline = function(text){
                                 this.text(text);
                                 this.html(this.html().replace(/\n/g,'<br/>'));
                                 return this;
                             }
-
+                            $("#chat-box").multiline("Dear <br/> Diary <br/> :D");
                             // Now you can do this:
                             //$("#chat-box").multiline('this\n has\n newlines');
                         });
