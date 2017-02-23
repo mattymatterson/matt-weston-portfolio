4540385f60a79d5c4581dd56e77062af9efb88d6
mattymatterson <matt.weston.mw@gmail.com>
2017-02-22 18:40:53 -0600

    Update chatrefresh.php

--- chatrefresh.php
+++ chatrefresh.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 $json = array();
+$fullChat = array();
 $dsn = "pgsql:"
     . "host=ec2-107-20-191-76.compute-1.amazonaws.com;"
     . "dbname=dc2ibd1t6ecgng;"
@@ -28,10 +29,10 @@ while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
     if ($row["chat_from"] == "3") {
         $name = "Logan McCourry";
     }
-    $json[] = $newtime . " " . $name. ": " . $row["message"] . "<br>";
+    $json[] = $newtime . " " . $name. ": " . $row["message"];
 }
-
+ 
 $fp = fopen('results.json', 'w');
-fwrite($fp, json_encode($json));
+fwrite($fp, json_encode($json[]);
 fclose($fp);
 ?>
