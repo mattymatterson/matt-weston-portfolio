3d7ae25376077941fc73298427d2db1aa280d2a7
mattymatterson <matt.weston.mw@gmail.com>
2017-02-23 21:37:34 -0600

    Update chatrefresh.php

--- chatrefresh.php
+++ chatrefresh.php
@@ -22,7 +22,7 @@ if (isset($_SESSION[chat_id])) {
         $newtime = substr($newtime[1], 0, -2); //removing AM/PM
         $hours = substr($newtime, 0, 2); //changing from GMT to CST
         $hours = $hours - 6; //changing from GMT to CST
-        if ($hours > 24) { $hours = $hours - 24; } //checking to make sure it doesnt have a weird number
+        if ($hours > 12) { $hours = $hours - 12; } //checking to make sure it doesnt have a weird number
         if ($hours < 0) { $hours = 12 + $hours; } //checking to make sure it doesnt have a weird number
         $newtime = $hours . substr($newtime, 2,6); //adding hours to the rest of the string
         if ($row["chat_from"] == "1") {
