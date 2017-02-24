9786b79009861b6e6367777dd990a61b3a49a83b
mattymatterson <matt.weston.mw@gmail.com>
2017-02-23 18:26:39 -0600

    Update studentScheduleLookup.php

--- studentScheduleLookup.php
+++ studentScheduleLookup.php
@@ -8,6 +8,7 @@ if(!isset($_SESSION['username'])) {
   $filepath = "/";
 
   // http headers for zip downloads
+  /*
   header("Pragma: public");
   header("Expires: 0");
   header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
@@ -17,7 +18,7 @@ if(!isset($_SESSION['username'])) {
   header("Content-Disposition: attachment; filename=\"".$filename."\"");
   header("Content-Transfer-Encoding: binary");
   header("Content-Length: ".filesize($filepath.$filename));
-  ob_end_flush();
+  ob_end_flush();*/
   @readfile($filepath.$filename);
 }
 ?>
