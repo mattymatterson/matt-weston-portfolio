b0d546acecf387eada9cbae9cf77cac1a6d60ac5
mattymatterson <matt.weston.mw@gmail.com>
2017-02-23 21:34:28 -0600

    Update chatrefresh.php

--- chatrefresh.php
+++ chatrefresh.php
@@ -23,7 +23,7 @@ if (isset($_SESSION[chat_id])) {
         $hours = substr($newtime, 0, 2); //changing from GMT to CST
         $hours = $hours - 6; //changing from GMT to CST
         if ($hours > 24) { $hours = $hours - 24; } //checking to make sure it doesnt have a weird number
-        if ($hours < 0) { $hours = 24 + $hours; } //checking to make sure it doesnt have a weird number
+        if ($hours < 0) { $hours = 12 + $hours; } //checking to make sure it doesnt have a weird number
         $newtime = $hours . substr($newtime, 2,6); //adding hours to the rest of the string
         if ($row["chat_from"] == "1") {
             $name = "Matt Weston";
